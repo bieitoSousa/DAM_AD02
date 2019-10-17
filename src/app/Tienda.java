1f0fcc43dc3969b3fee7c1d46110487b196ed149
Bieito <bieito.sousa.1991@gmail.com>
2019-10-18 00:08:47 +0200

    cambios

---
+++ src/app/Tienda.java
@@ -0,0 +1,63 @@
+package app;
+
+import java.util.ArrayList;
+
+public class Tienda extends Compañia{
+Tienda t = null;
+ ArrayList clientList;
+ ArrayList productosList;
+ ArrayList empleadosList;
+ 
+
+
+	public Tienda(String nom, String cidade) {
+	}
+
+	public static Tienda cargarTienda() {
+		return null;
+	}
+
+
+
+	public void deleteTienda(String nom) {
+	}
+
+	public void addProducto(String idProducto) {
+	}
+
+	public void setProducto(String idProducto, String descripción, int prezo, int cantidade) {
+	}
+
+	public void deleteProducto(String idProducto) {
+	}
+
+
+
+	public void viewEmpregadoList() {
+	}
+
+	public void viewProductosList() {
+	}
+
+	public void addEmpregado(String nomEmp, String apelEmp) {
+	}
+
+	public void setEmpleado(String nomEmp, String apelEmp) {
+	}
+
+	public void deleteEmpregado(String nomEmp) {
+	}
+
+	public void viewClienteList() {
+	}
+
+	public void addCliente(String nomCli, String apelCli, String mailCli) {
+	}
+
+	public void deleteCliente(String nomCli) {
+	}
+
+	public void backup() {
+	}
+
+}
